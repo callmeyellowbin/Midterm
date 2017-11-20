8ac3f4fbc014365569db75fe68b5f13e77c23ab5
huanglu20124 <845758437@qq.com>
2017-11-20 12:02:37 +0800

    2017.11.20数据接入

---
+++ app/src/main/java/com/lxc/midterm/Const.java
@@ -0,0 +1,10 @@
+package com.lxc.midterm;
+
+/**
+ * Created by road on 2017/11/19.
+ */
+
+public class Const {
+    public static final String IP  = "https://sysufade.cn/three_kingdoms/";
+    public static final String WEBSOCKET_URI  = "ws://119.23.229.19:8080/three_kingdoms/webSocketServer.action";
+}
